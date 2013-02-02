5d7c58c2f8b5b81aa0c8b2e0b3e681d5465a6395
Peranikov <ym.nekoze@gmail.com>
2013-02-02 16:22:12 +0900

    first commit

---
+++ HelloAndroid/src/com/peranikov/helloandroid/MainActivity.java
@@ -0,0 +1,29 @@
+package com.peranikov.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void onClickHelloWorld(View view){
+    	EditText editText = (EditText)findViewById(R.id.editText);
+    	editText.setText("Hello World! :-)");
+    }
+    
+}
